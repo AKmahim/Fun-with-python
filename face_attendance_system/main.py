57ddacc3e4c01edd9c815054bc43fc0c762a3209
AKmahim <mdmahimhossen134@gmail.com>
2023-12-03 21:04:24 +0600

    face attendance complete

--- face_attendance_system/main.py
+++ face_attendance_system/main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import os.path
 import datetime
 import pickle
+import subprocess
+
 
 import cv2
 from PIL import Image,ImageTk
@@ -27,6 +29,12 @@ class App:
 
         self.add_webcam(self.webcam_label)
 
+        self.db_dir = './db'
+        if not os.path.exists(self.db_dir):
+            os.mkdir(self.db_dir)
+
+        self.log_path = './log.txt'        
+
     def add_webcam(self,label):
         if 'cap' not in self.__dict__:
             self.cap = cv2.VideoCapture(0)
@@ -48,15 +56,73 @@ class App:
 
 
     def login(self):
-        pass
+
+        unknown_img_path = './.tmp.jpg'
+
+        cv2.imwrite(unknown_img_path,self.most_recent_capture_arr)
+        output = str(subprocess.check_output(['face_recognition',self.db_dir,unknown_img_path]))
+        # print(output)
+        name = output.split(',')[1][:-3]
+        print(name)
+        if name in ['unknown_person', 'no_persons_found']:
+                util.msg_box('Ups...', 'Unknown user. Please register new user or try again.')
+        else:
+            util.msg_box('Welcome back !', 'Welcome, {}.'.format(name))
+            with open(self.log_path, 'a') as f:
+                    f.write('{},{},in\n'.format(name, datetime.datetime.now()))
+                    f.close()
+        #remove temp image from tmp file
+        os.remove(unknown_img_path)
 
     def register_new_user(self):
-        pass
+        self.register_new_user_window = tk.Toplevel(self.main_window)
+        self.register_new_user_window.geometry("1200x520+370+120")
+        
+        self.accept_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Accept', 'green', self.accept_register_new_user)
+        self.accept_button_register_new_user_window.place(x=750, y=300)
+
+        self.try_again_button_register_new_user_window = util.get_button(self.register_new_user_window, 'Try again', 'red', self.try_again_register_new_user)
+        self.try_again_button_register_new_user_window.place(x=750, y=400)
+
+        self.capture_label = util.get_img_label(self.register_new_user_window)
+        self.capture_label.place(x=10, y=0, width=700, height=500)
+
+        self.add_img_to_label(self.capture_label)
+
+        self.entry_text_register_new_user = util.get_entry_text(self.register_new_user_window)
+        self.entry_text_register_new_user.place(x=750, y=150)
+
+        self.text_label_register_new_user = util.get_text_label(self.register_new_user_window, 'Please, \ninput username:')
+        self.text_label_register_new_user.place(x=750, y=70)
+
+
+
+    def try_again_register_new_user(self):
+        self.register_new_user_window.destroy()
+        
+
+    def add_img_to_label(self,label):
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+        label.imgtk = imgtk
+        label.configure(image=imgtk)
+
+        self.register_new_user_capture = self.most_recent_capture_arr.copy()
+
 
     def start(self):
         self.main_window.mainloop()
         
-    
+    def accept_register_new_user(self):
+        name = self.entry_text_register_new_user.get(1.0, "end-1c").strip()  
+        file_path = os.path.join(self.db_dir, f'{name}.jpg')
+        cv2.imwrite(file_path, self.register_new_user_capture)
+        
+
+        util.msg_box('Success!', 'User was registered successfully !')
+        self.register_new_user_window.destroy()
+
+
+
 
 if __name__ == "__main__":
     app = App()
