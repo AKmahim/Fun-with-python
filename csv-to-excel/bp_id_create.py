4c97dd13230923c34042a0c99d8d8b123c77e48a
AKmahim <mdmahimhossen134@gmail.com>
2024-05-16 09:47:35 +0600

    changes in csv-to-excel

--- csv-to-excel/bp_id_create.py
+++ csv-to-excel/bp_id_create.py
@@ -1,54 +1,54 @@
-import pandas as pd
-import requests
-import time
+# import pandas as pd
+# import requests
+# import time
 
-# Read the Excel file into a pandas DataFrame
-excel_file = "BP-2.xlsx"
-df = pd.read_excel(excel_file)
+# # Read the Excel file into a pandas DataFrame
+# excel_file = "BP-2.xlsx"
+# df = pd.read_excel(excel_file)
 
-# API endpoint
-api_url = "http://127.0.0.1:8000/api/register"
+# # API endpoint
+# api_url = "http://127.0.0.1:8000/api/register"
 
-# Rate limiting parameters
-users_per_minute = 40
-time_interval = 60 / users_per_minute
+# # Rate limiting parameters
+# users_per_minute = 40
+# time_interval = 60 / users_per_minute
 
-# Iterate over rows of the DataFrame
-for index, row in df.iterrows():
-    # Extract data from the current row
-    name = row['Name']
-    # print(name)
-    phone_number = str(row['Phone'])  # Convert to string if needed
-    bp_id = row['bp_id']
-    password = row['password']
-    area = row['Area']  # Assuming 'Area' maps to 'working_district'
+# # Iterate over rows of the DataFrame
+# for index, row in df.iterrows():
+#     # Extract data from the current row
+#     name = row['Name']
+#     # print(name)
+#     phone_number = str(row['Phone'])  # Convert to string if needed
+#     bp_id = row['bp_id']
+#     password = row['password']
+#     area = row['Area']  # Assuming 'Area' maps to 'working_district'
 
-    # Adding '0' prefix to phone number if it's missing
-    if not phone_number.startswith('0'):
-        phone_number = '0' + phone_number
+#     # Adding '0' prefix to phone number if it's missing
+#     if not phone_number.startswith('0'):
+#         phone_number = '0' + phone_number
 
 
-    # Form data for the POST request
-    form_data = {
-        'name': name,
-        'phone_number': phone_number,
-        'bp_id': bp_id,
-        'password': password,
-        'working_district': area  # Assuming 'Area' maps to 'working_district'
-    }
+#     # Form data for the POST request
+#     form_data = {
+#         'name': name,
+#         'phone_number': phone_number,
+#         'bp_id': bp_id,
+#         'password': password,
+#         'working_district': area  # Assuming 'Area' maps to 'working_district'
+#     }
 
-    # Send POST request to the API
-    response = requests.post(api_url, data=form_data)
+#     # Send POST request to the API
+#     response = requests.post(api_url, data=form_data)
 
-    # Check response status
-    if response.status_code == 200:
-        print(f"User {name}, {bp_id} created successfully.")
-    else:
-        print(f"Failed to create user {name},{bp_id}. Status code: {response.status_code}")
-        # print(f"error: {response.error}")
+#     # Check response status
+#     if response.status_code == 200:
+#         print(f"User {name}, {bp_id} created successfully.")
+#     else:
+#         print(f"Failed to create user {name},{bp_id}. Status code: {response.status_code}")
+#         # print(f"error: {response.error}")
 
-    # Rate limiting: wait for the specified time interval before proceeding to the next iteration
-    time.sleep(time_interval)
+#     # Rate limiting: wait for the specified time interval before proceeding to the next iteration
+#     time.sleep(time_interval)
 
 
 
@@ -74,3 +74,65 @@ for index, row in df.iterrows():
 # output_file = 'BP.csv'  # Replace 'output.csv' with your desired CSV file name
 
 # excel_to_csv(input_file, output_file)
+
+
+
+
+# ============================================= login test =================================
+
+import pandas as pd
+import requests
+import time
+
+# Read the Excel file into a pandas DataFrame
+excel_file = "BP-2.xlsx"
+df = pd.read_excel(excel_file)
+
+# API endpoint
+api_url = "https://expactivation.app/api/v1/login"
+
+# Rate limiting parameters
+users_per_minute = 40
+time_interval = 60 / users_per_minute
+
+# Create an empty list to store problematic data
+problematic_data_list = []
+
+# Iterate over rows of the DataFrame
+for index, row in df.iterrows():
+    # Extract data from the current row
+    name = row['Name']
+    phone_number = str(row['Phone'])  # Convert to string if needed
+    bp_id = row['bp_id']
+    password = row['password']
+    area = row['Area']  # Assuming 'Area' maps to 'working_district'
+
+    # Adding '0' prefix to phone number if it's missing
+    if not phone_number.startswith('0'):
+        phone_number = '0' + phone_number
+
+    # Form data for the POST request
+    form_data = {
+        'bp_id': bp_id,
+        'password': password,
+    }
+
+    # Send POST request to the API
+    response = requests.post(api_url, data=form_data)
+
+    # Check response status
+    if response.status_code == 200:
+        print(f"User login {name}, {bp_id}, {password} successfully.")
+    elif response.status_code == 401:
+        print(f"Failed to Login---> {name},{bp_id},{password}.")
+        problematic_data_list.append({'Name': name, 'bp_id': bp_id, 'password': password})
+    else :
+        print(f"Fail and got error.{name},{bp_id},{password}. status code = {response.status_code}")
+
+    # Rate limiting: wait for the specified time interval before proceeding to the next iteration
+    time.sleep(time_interval)
+
+# Save problematic data to CSV file
+problematic_data = pd.DataFrame(problematic_data_list)
+
+# Save problematic data to CSV file
